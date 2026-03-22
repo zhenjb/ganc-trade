59b2501faa66b681db343eb69d38c2967f465ca9
zjqz-scyl. Le Trong Nghia <120289465+zjqingzun@users.noreply.github.com>
2026-03-22 13:06:25 +0700

    Revert "[dev-ttn] Implement MsgWithdraw + unit tests (mock)"

--- sw/ob/x/dex/types/errors.go
+++ sw/ob/x/dex/types/errors.go
@@ -8,7 +8,5 @@ import (
 
 // x/dex module sentinel errors
 var (
-	ErrInvalidSigner  = errors.Register(ModuleName, 1100, "expected gov account as only signer for proposal message")
-	ErrInvalidAddress = errors.Register(ModuleName, 1101, "invalid address")
-	ErrInvalidAmount  = errors.Register(ModuleName, 1102, "invalid amount")
+	ErrInvalidSigner = errors.Register(ModuleName, 1100, "expected gov account as only signer for proposal message")
 )
