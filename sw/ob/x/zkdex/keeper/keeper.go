1b736c18ad28d57874a16901ec3c5aecc5d4dfcb
nguyenmy2402 <myn047450@gmail.com>
2026-05-11 21:35:13 +0700

    ONCHAIN-01 Scaffold x/zkdex

---
+++ sw/ob/x/zkdex/keeper/keeper.go
@@ -0,0 +1,67 @@
+package keeper
+
+import (
+	"fmt"
+
+	"cosmossdk.io/collections"
+	"cosmossdk.io/core/address"
+	corestore "cosmossdk.io/core/store"
+	"github.com/cosmos/cosmos-sdk/codec"
+
+	"ob/x/zkdex/types"
+)
+
+type Keeper struct {
+	storeService corestore.KVStoreService
+	cdc          codec.Codec
+	addressCodec address.Codec
+	// Address capable of executing a MsgUpdateParams message.
+	// Typically, this should be the x/gov module account.
+	authority []byte
+
+	Schema collections.Schema
+	Params collections.Item[types.Params]
+
+	bankKeeper types.BankKeeper
+	authKeeper types.AuthKeeper
+}
+
+func NewKeeper(
+	storeService corestore.KVStoreService,
+	cdc codec.Codec,
+	addressCodec address.Codec,
+	authority []byte,
+
+	bankKeeper types.BankKeeper,
+	authKeeper types.AuthKeeper,
+) Keeper {
+	if _, err := addressCodec.BytesToString(authority); err != nil {
+		panic(fmt.Sprintf("invalid authority address %s: %s", authority, err))
+	}
+
+	sb := collections.NewSchemaBuilder(storeService)
+
+	k := Keeper{
+		storeService: storeService,
+		cdc:          cdc,
+		addressCodec: addressCodec,
+		authority:    authority,
+
+		bankKeeper: bankKeeper,
+		authKeeper: authKeeper,
+		Params:     collections.NewItem(sb, types.ParamsKey, "params", codec.CollValue[types.Params](cdc)),
+	}
+
+	schema, err := sb.Build()
+	if err != nil {
+		panic(err)
+	}
+	k.Schema = schema
+
+	return k
+}
+
+// GetAuthority returns the module's authority.
+func (k Keeper) GetAuthority() []byte {
+	return k.authority
+}
