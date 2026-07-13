baf5c76f300909fcfbed16d3c2d13522811d3b29
nguyenmy2402 <myn047450@gmail.com>
2026-07-14 00:43:13 +0700

    fix Nhánh 1 - Denom

--- test/order/matching@balance.sh
+++ test/order/matching@balance.sh
@@ -1,7 +1,7 @@
 # REGISTER
-obd tx dex register-pairs ATOM USDT 0.01 1 --from alice --chain-id ob -y
+obd tx dex register-pairs uatom uusdc 0.01 1 --from alice --chain-id ob -y
 sleep 1
-obd tx dex register-pairs USDT ATOM 0.01 1 --from alice --chain-id ob -y
+obd tx dex register-pairs uusdc uatom 0.01 1 --from alice --chain-id ob -y
 sleep 1
 
 
@@ -22,7 +22,7 @@ sleep 2
 
 
 # SELL
-obd tx dex place-order ATOM-USDT SELL 9 10 --from bob -y 
+obd tx dex place-order uatom-uusdc SELL 9 10 --from bob -y 
 sleep 2
 
 
@@ -34,9 +34,9 @@ sleep 2
 
 
 # BUY
-obd tx dex place-order ATOM-USDT BUY 1 10 --from alice -y
+obd tx dex place-order uatom-uusdc BUY 1 10 --from alice -y
 sleep 2
-obd tx dex place-order ATOM-USDT BUY 9 10 --from alice -y
+obd tx dex place-order uatom-uusdc BUY 9 10 --from alice -y
 sleep 2
 
 
